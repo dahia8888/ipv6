24490158549862acda98622ee278e74f13fd7d2e
dahia8888 <139503336+dahia8888@users.noreply.github.com>
2024-01-02 17:53:51 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -11,13 +11,10 @@ IP_ADDRESSES=($(hostname -I))
 install_xray() {
 	echo "安装 Xray..."
 	apt-get install unzip -y || yum install unzip -y
-	#wget https://183.230.82.107/root/Xray-linux-64.zip
-	#wget https://github.com/XTLS/Xray-core/releases/download/v1.8.3/Xray-linux-64.zip
 	if [ ! -f "/root/Xray-linux-64.zip" ];then
 	echo "Xray  soft isnt exist!"
-        wget https://f.ws59.cn/f/cw9rrvlg8ns/Xray-linux-64.zip
+        wget https://github.com/XTLS/Xray-core/releases/download/v1.8.3/Xray-linux-64.zip
 	fi	
-	#wget http://deershare.com/recv/825658
 	unzip Xray-linux-64.zip
 	mv xray /usr/local/bin/xrayL
 	chmod +x /usr/local/bin/xrayL
